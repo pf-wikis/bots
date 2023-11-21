f44dd0ce1252b14fb07bd525acd43066da19cf05
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2023-11-21 09:58:11 +0100

    Add meta bot

---
+++ src/main/java/io/github/pfwikis/bots/meta/Meta.java
@@ -0,0 +1,52 @@
+package io.github.pfwikis.bots.meta;
+
+import com.beust.jcommander.Parameters;
+
+import io.github.classgraph.ClassGraph;
+import io.github.pfwikis.bots.Runner;
+import io.github.pfwikis.bots.common.bots.Bot;
+import io.github.pfwikis.bots.common.bots.SimpleBot;
+
+@Parameters(commandDescription = "Metabot that updates the main bots description.")
+public class Meta extends SimpleBot {
+
+	public Meta() {
+		super("meta", "VirenerusBot");
+	}
+	
+	@Override
+	protected String getDescription() {
+		var scan = new ClassGraph()
+			.acceptPackages(Runner.class.getPackageName())
+			.enableClassInfo()
+			.scan();
+		var bots = scan.getSubclasses(Bot.class)
+			.stream()
+			.filter(bc->!bc.isAbstract())
+			.map(bc-> {
+				try {
+					return bc.loadClass(Bot.class)
+						.getConstructor()
+						.newInstance();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			})
+			.map(b->"{{User:"+b.getBotName()+"/Status}}")
+			.toList();
+				
+		return """
+		This bot is owned by [[User:Virenerus]]. It is a manager bot that is responsible for managing multiple thematic sub bots, you might know.
+
+		==Sub bots==
+		{| class="wikitable" style="margin:auto"
+		|-
+		! Bot Name !! Last run !! Status !! notes
+		%s
+		|}
+		""".formatted(bots);
+	}
+
+	@Override
+	public void run() throws Exception {}
+}
