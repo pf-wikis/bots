ce8035e427ca249a9ed826a119c0e62f310b67e1
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2024-01-25 17:09:14 +0100

    Better serialized year handling

--- src/main/java/io/github/pfwikis/bots/factshelper/FormDefinition.java
+++ src/main/java/io/github/pfwikis/bots/factshelper/FormDefinition.java
@@ -53,22 +53,22 @@ public class FormDefinition {
 		//add special autoproperties
 		result.add(props.get("Fact type").withGenerateCode("Template:Facts/"+name));
 		//Add a release year if if there is a release date
+		String releaseYearDefault = "";
+		if(properties.contains("Serialized")) {
+			releaseYearDefault = "|default={{#rmatch:{{{Serialized|}}}|.*?([0-9]{4}).*|${1}|}}";
+		}
 		if(properties.contains("Release date")) {
-			result.add(props.get("Release year").withGenerateCode("{{#show:{{FULLPAGENAME}}|?Release date#-F[Y]=|sort=Release date|order=ASC|limit=1|mainlabel=-|searchlabel=}}"));
+			result.add(props.get("Release year").withGenerateCode("{{#show:{{FULLPAGENAME}}|?Release date#-F[Y]=|sort=Release date|order=ASC|limit=1|mainlabel=-|searchlabel="+releaseYearDefault+"}}"));
 		}
 		else if(subForms.stream().anyMatch(s->s.getProperties().contains("Release date"))){
 			var sub=subForms.stream().filter(s->s.getProperties().contains("Release date")).findAny().get();
-			result.add(props.get("Release year").withGenerateCode("{{#ask:[[-Has subobject::{{FULLPAGENAME}}]][[Fact type::Template:Facts/"+name+"/"+sub.name+"]][[Release date::+]]|?Release date#-F[Y]=|sort=Release date|order=ASC|limit=1|mainlabel=-|searchlabel=}}"));
+			result.add(props.get("Release year").withGenerateCode("{{#ask:[[-Has subobject::{{FULLPAGENAME}}]][[Fact type::Template:Facts/"+name+"/"+sub.name+"]][[Release date::+]]|?Release date#-F[Y]=|sort=Release date|order=ASC|limit=1|mainlabel=-|searchlabel="+releaseYearDefault+"}}"));
 			result.add(props.get("Release date").withGenerateCode("{{#ask:[[-Has subobject::{{FULLPAGENAME}}]][[Fact type::Template:Facts/"+name+"/"+sub.name+"]][[Release date::+]]|?Release date#ISO-P=|sort=Release date|order=ASC|limit=1|mainlabel=-|searchlabel=}}"));
 			result.add(props.get("Release date precision").withGenerateCode("{{#ask:[[-Has subobject::{{FULLPAGENAME}}]][[Fact type::Template:Facts/"+name+"/"+sub.name+"]][[Release date::+]]|?Release date precision=|sort=Release date|order=ASC|limit=1|mainlabel=-|searchlabel=|default=empty}}"));
 		}
 		//add gallery page
 		if(properties.contains("Gallery")) {
 			result.add(props.get("Gallery page").withGenerateCode("{{#if:{{{Gallery|}}}|Category:Artwork from {{{Gallery}}}|Category:Artwork from {{PAGENAME}}}}"));
-		}
-		//serialized year
-		if(properties.contains("Serialized")) {
-			result.add(props.get("Serialized year").withGenerateCode("{{#rmatch:{{{Serialized|}}}|.*?([0-9]{4}).*|${1}|empty}}"));
 		}
 		
 		return result;
