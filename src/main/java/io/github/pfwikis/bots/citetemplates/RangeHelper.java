2f3ce65e29da742f40d2ef2fe117ef01d642eb53
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2024-04-02 17:37:27 +0200

    CiteTemplate: Reduce need for multiple span duplicates

---
+++ src/main/java/io/github/pfwikis/bots/citetemplates/RangeHelper.java
@@ -0,0 +1,24 @@
+package io.github.pfwikis.bots.citetemplates;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeSet;
+import com.google.common.collect.TreeRangeSet;
+
+public class RangeHelper {
+
+	public static RangeSet<Integer> toHasValueRanges(Map<String, List<Range<Integer>>> ranges) {
+		var result = TreeRangeSet.<Integer>create();
+		for(var e:ranges.entrySet()) {
+			if(StringUtils.isNotBlank(e.getKey())) {
+				result.addAll(e.getValue());
+			}
+		}
+		return result;
+	}
+
+}
