6d848ede130cc3205871e0249bfd1077689e6576
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2024-08-21 10:46:21 +0900

    Improve healthcheck exception handling

--- src/main/java/io/github/pfwikis/bots/healthcheck/HealthCheck.java
+++ src/main/java/io/github/pfwikis/bots/healthcheck/HealthCheck.java
@@ -1,21 +1,21 @@
 package io.github.pfwikis.bots.healthcheck;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
 import java.security.cert.X509Certificate;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.HttpsURLConnection;
 
 import org.apache.commons.lang3.time.DurationFormatUtils;
 
 import com.beust.jcommander.Parameters;
+import com.google.common.util.concurrent.Uninterruptibles;
 
 import io.github.pfwikis.bots.common.Discord;
 import io.github.pfwikis.bots.scheduler.Schedulable;
@@ -47,29 +47,49 @@ public class HealthCheck extends Schedulable {
 	private static Duration WARNING_PERIOD = Duration.ofDays(7);
 	
 	private void test(String url, String domain) {
+		int tryCount = 1;
 		try {
-			var cert = getCertificate(url, domain);
-			var notAfter = cert.getNotAfter().toInstant();
-			var inOneWeek = Instant.now().plus(WARNING_PERIOD);
-			
-			if(inOneWeek.isBefore(notAfter)) {
-				log.info("{} valid until {}", url, cert.getNotAfter());
-			}
-			else {
-				log.error("{} only valid until {}", url, cert.getNotAfter());
-				discord.report(
-					"**Certificate Warning: **the certificate for "
-					+ url
-					+ " will expire in "
-					+ DurationFormatUtils.formatDurationWords(
-						Duration.between(Instant.now(), notAfter).truncatedTo(ChronoUnit.MINUTES).toMillis(),
-						true,
-						true
-					)
-					+ "! Something is wrong with the renewal system.");
+			boolean retry  = true;
+			while(retry) {
+				try {
+					retry = false;
+					tryTest(url, domain);
+				} catch (ConnectException e) {
+					if(tryCount++>6)
+						throw e;
+					else {
+						retry = true;
+						Uninterruptibles.sleepUninterruptibly(10, TimeUnit.SECONDS);
+					}
+				}
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			discord.report("**Unexpected exception during health check:**\n"
+					+e.getClass()+" "
+					+e.getMessage());
+		}
+	}
+
+	private void tryTest(String url, String domain) throws MalformedURLException, IOException {
+		var cert = getCertificate(url, domain);
+		var notAfter = cert.getNotAfter().toInstant();
+		var inOneWeek = Instant.now().plus(WARNING_PERIOD);
+		
+		if(inOneWeek.isBefore(notAfter)) {
+			log.info("{} valid until {}", url, cert.getNotAfter());
+		}
+		else {
+			log.error("{} only valid until {}", url, cert.getNotAfter());
+			discord.report(
+				"**Certificate Warning: **the certificate for "
+				+ url
+				+ " will expire in "
+				+ DurationFormatUtils.formatDurationWords(
+					Duration.between(Instant.now(), notAfter).truncatedTo(ChronoUnit.MINUTES).toMillis(),
+					true,
+					true
+				)
+				+ "! Something is wrong with the renewal system.");
 		}
 	}
 
