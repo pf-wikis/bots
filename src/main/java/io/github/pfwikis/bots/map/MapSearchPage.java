c1383eca98b3416886ade65fb1ae3e2d31eb5a25
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2025-03-14 19:34:16 +0100

    Adapt MapSearchPage bot to new format

--- src/main/java/io/github/pfwikis/bots/map/MapSearchPage.java
+++ src/main/java/io/github/pfwikis/bots/map/MapSearchPage.java
@@ -1,9 +1,11 @@
 package io.github.pfwikis.bots.map;
 
+import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import io.github.pfwikis.bots.common.Wiki;
@@ -23,22 +25,29 @@ public class MapSearchPage extends SimpleBot {
 		
 		var sb = new StringBuilder()
 				.append("<noinclude>{{Bot created|").append(this.getBotName()).append("}}\n")
-				.append("This page is a helper for the byName parameter.\n")
+				.append("This page is a helper for the by name parameter.\n")
 				.append("The full list of entries are:\n<ul>\n");
-		var entries = Jackson.JSON.readValue(URI.create("https://map.pathfinderwiki.com/search.json").toURL(), Entry[].class);
-		for(var e:entries) {
-			sb.append("<li>").append(e.label).append("</li>\n");
+		var categories = Jackson.JSON.readValue(new File("E:\\workspace-paizo\\mapping\\frontend\\public\\search.json"), Category[].class);
+		//var categories = Jackson.JSON.readValue(URI.create("https://map.pathfinderwiki.com/search.json").toURL(), Category[].class);
+		for(var cat:categories) {
+			sb.append("<li>").append(cat.category).append("</li>\n<ul>\n");
+			for(var e:cat.entries) {
+				sb.append("<li>").append(e.label).append("</li>\n");
+			}
+			sb.append("</ul>\n");
 		}
-		sb.append("<ul></noinclude><includeonly>{{#switch:{{{1}}}");
-		for(var e:entries) {
-			sb
-				.append("|")
-				.append(e.label)
-				.append("=")
-				.append(Arrays.stream(e.bbox)
+		sb.append("</ul></noinclude><includeonly>{{#switch:{{{1}}}");
+		for(var cat:categories) {
+			for(var e:cat.entries) {
+				sb
+					.append("|")
+					.append(e.label)
+					.append("=")
+					.append(Arrays.stream(e.bbox)
 						.map(BigDecimal::toPlainString)
 						.collect(Collectors.joining(","))
-				);
+					);
+			}
 		}
 		sb.append("|}}</includeonly>");
 		
@@ -56,5 +65,6 @@ public class MapSearchPage extends SimpleBot {
 		""".formatted(run.getServer().getName());
 	}
 
-	private record Entry(String label, BigDecimal[] bbox){}
+	record Category(String category, List<Result> entries) {}
+	record Result(String label, BigDecimal[] bbox) {}
 }
