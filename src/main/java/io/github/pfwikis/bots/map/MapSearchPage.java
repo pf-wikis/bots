af5e0646c92f642dd1a8b8f9525013018040ad63
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2025-07-16 23:29:21 +0200

    Add categorize keys to the map search page

--- src/main/java/io/github/pfwikis/bots/map/MapSearchPage.java
+++ src/main/java/io/github/pfwikis/bots/map/MapSearchPage.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.HashMultiset;
+
 import io.github.pfwikis.bots.common.Wiki;
 import io.github.pfwikis.bots.common.bots.RunContext;
 import io.github.pfwikis.bots.common.bots.SimpleBot;
@@ -27,20 +29,35 @@ public class MapSearchPage extends SimpleBot {
 				.append("<noinclude>{{Bot created|").append(this.getBotName()).append("}}\n")
 				.append("This page is a helper for the by name parameter.\n")
 				.append("The full list of entries are:\n<ul>\n");
-		var categories = Jackson.JSON.readValue(URI.create("https://map.pathfinderwiki.com/search.json").toURL(), Category[].class);
+		var categories = Arrays.asList(Jackson.JSON.readValue(URI.create("https://map.pathfinderwiki.com/search.json").toURL(), Category[].class));
+		var labelCounts = HashMultiset.<String>create();
+		categories.forEach(cat->cat.entries.forEach(e->labelCounts.add(e.label)));
+		
 		for(var cat:categories) {
-			sb.append("<li>").append(cat.category).append("</li>\n<ul>\n");
+			sb.append("<li>").append(cat.category).append("</li>\n<ul style=\"column-width: 20rem;\">\n");
 			for(var e:cat.entries) {
-				sb.append("<li>").append(e.label).append("</li>\n");
+				sb.append("<li>").append(e.label);
+				if(labelCounts.count(e.label)>1) {
+					sb.append(" (via <code>").append(e.label).append(";").append(cat.category).append("</code>)");
+				}
+				sb.append("</li>\n");
 			}
 			sb.append("</ul>\n");
 		}
 		sb.append("</ul></noinclude><includeonly>{{#switch:{{{1}}}");
 		for(var cat:categories) {
 			for(var e:cat.entries) {
+				var key = e.label;
+				if(labelCounts.count(e.label)>1) {
+					key+=";"+cat.category;
+				}
+				
+				if(key.contains("=") || key.contains("|"))
+					throw new IllegalStateException("Illegal key "+key);
+				
 				sb
 					.append("|")
-					.append(e.label)
+					.append(key)
 					.append("=")
 					.append(Arrays.stream(e.bbox)
 						.map(BigDecimal::toPlainString)
