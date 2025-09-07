84ac1a1d10712cebbdf6d9cbebd656f52c200071
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2025-09-07 21:49:31 +0200

    Add Area template generation to to bots

--- src/main/java/io/github/pfwikis/bots/map/MapSearchPage.java
+++ src/main/java/io/github/pfwikis/bots/map/MapSearchPage.java
@@ -1,6 +1,5 @@
 package io.github.pfwikis.bots.map;
 
-import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -27,46 +26,89 @@ public class MapSearchPage extends SimpleBot {
 	public void run(RunContext ctx) throws IOException {
 		if(run.getServer() == Wiki.SF) return;
 		
-		var sb = new StringBuilder()
-				.append("<noinclude>{{Bot created|").append(this.getBotName()).append("}}\n")
-				.append("This page is a helper for the by name parameter.\n")
-				.append("The full list of entries are:\n<ul>\n");
-		var categories = Arrays.asList(Jackson.JSON.readValue(URI.create("https://map.pathfinderwiki.com/search.json").toURL(), Category[].class));
+		var categoriesIn = Arrays.asList(Jackson.JSON.readValue(URI.create("https://map.pathfinderwiki.com/search.json").toURL(), CategoryIn[].class));
 		var labelCounts = HashMultiset.<String>create();
-		categories.forEach(cat->cat.entries.forEach(e->labelCounts.add(e.label)));
+		categoriesIn.forEach(cat->cat.entries.forEach(e->labelCounts.add(e.label)));
 		
-		for(var cat:categories) {
-			sb.append("<li>").append(cat.category).append("</li>\n<ul style=\"column-width: 20rem;\">\n");
-			for(var e:cat.entries) {
-				sb.append("<li>").append(e.label);
-				if(labelCounts.count(e.label)>1) {
-					sb.append(" (via <code>").append(key(labelCounts, e.label, cat.category)).append("</code>)");
+		var categories = categoriesIn.stream()
+				.map(c->new Category(
+						c.category,
+						c.entries.stream()
+							.map(r->new Result(
+									key(labelCounts, r.label, c.category),
+									r.label,
+									labelCounts.count(r.label),
+									r.bbox,
+									r.areaM2
+							))
+							.toList()
+				))
+				.toList();
+		
+		
+		createSearch(categories);
+		createSearchAspect(categories);
+		createArea(categories);
+	}
+	
+	private void createArea(List<Category> categories) {
+		var sb = new StringBuilder()
+			.append("<noinclude>{{Bot created|").append(this.getBotName()).append("}}\n")
+			.append("""
+				{{Documentation|content=
+				<wikitext doc>{{Area|Ustalav}}</wikitext>
+				<templatedata>
+				{
+					"params": {
+						"1": {
+							"label": "area",
+							"description": "Nation or geographical feature",
+							"type": "string",
+							"required": true
+						}
+					},
+					"format": "inline"
 				}
-				sb.append("</li>\n");
-			}
-			sb.append("</ul>\n");
-		}
-		sb.append("</ul></noinclude><includeonly>{{#switch:{{{1}}}");
+				</templatedata>
+				}}[[Category:Templates]]
+				</noinclude><includeonly>{{#if:{{Area/Helper|{{{1}}}}}|{{formatnum:{{Area/Helper|{{{1}}}}}}} mi²}}{{Validate parameters}}</includeonly>
+				""");
+		run.getWiki().editIfChange("Template:Area", sb.toString(), "Automatic update");
+		sb = new StringBuilder()
+			.append("<noinclude>[[Category:Helper Template]]</noinclude><includeonly>")
+			.append("{{#switch:{{{1}}}");
 		for(var cat:categories) {
 			for(var e:cat.entries) {
+				if(e.areaM2 == null) continue;
+				
+				var area = new BigDecimal(0.000000386102d*e.areaM2);
+				if(area.doubleValue()>100) {
+					area = area.setScale(0,RoundingMode.HALF_UP);
+				}
+				else if(area.doubleValue()>10) {
+					area = area.setScale(1,RoundingMode.HALF_UP);
+				}
+				else {
+					area = area.setScale(2,RoundingMode.HALF_UP);
+				}
+				if(area.compareTo(BigDecimal.ZERO) == 0)
+					continue;
+				
 				sb
-					.append("|")
-					.append(key(labelCounts, e.label, cat.category))
+					.append("\n|")
+					.append(e.key)
 					.append("=")
-					.append(Arrays.stream(e.bbox)
-						.map(BigDecimal::toPlainString)
-						.collect(Collectors.joining(","))
-					);
+					.append(area.toPlainString());
 			}
 		}
-		sb.append("|}}</includeonly>");
-		
-		
-		run.getWiki().editIfChange("Template:DisplayMap/Search", sb.toString(), "Automatic update");
-		
-		
-		sb = new StringBuilder();
-		sb.append("<noinclude>This page works like {{tl|DisplayMap/Search}}, "
+		sb.append("\n}}</includeonly>");
+		run.getWiki().editIfChange("Template:Area/Helper", sb.toString(), "Automatic update");
+	}
+	
+	private void createSearchAspect(List<Category> categories) {
+		var sb = new StringBuilder()
+			.append("<noinclude>{{Bot created|").append(this.getBotName()).append("}}\n")
+			.append("This page works like {{tl|DisplayMap/Search}}, "
 				+ "but generetes the aspect ratio instead of a bbox.</noinclude><includeonly>{{#switch:{{{1}}}");
 		for(var cat:categories) {
 			for(var e:cat.entries) {
@@ -82,7 +124,7 @@ public class MapSearchPage extends SimpleBot {
 				
 				sb
 					.append("|")
-					.append(key(labelCounts, e.label, cat.category))
+					.append(e.key)
 					.append("=")
 					.append(new BigDecimal(width/height).setScale(2,RoundingMode.HALF_UP).toPlainString());
 			}
@@ -91,9 +133,45 @@ public class MapSearchPage extends SimpleBot {
 		
 		
 		run.getWiki().editIfChange("Template:DisplayMap/Search aspect", sb.toString(), "Automatic update");
-		
 	}
-	
+
+	private void createSearch(List<Category> categories) {
+		var sb = new StringBuilder()
+				.append("<noinclude>{{Bot created|").append(this.getBotName()).append("}}\n")
+				.append("This page is a helper for the by name parameter.\n")
+				.append("The full list of entries are:\n<ul>\n");
+		
+		
+		for(var cat:categories) {
+			sb.append("<li>").append(cat.category).append("</li>\n<ul style=\"column-width: 20rem;\">\n");
+			for(var e:cat.entries) {
+				sb.append("<li>").append(e.label);
+				if(e.labelCount>1) {
+					sb.append(" (via <code>").append(e.key).append("</code>)");
+				}
+				sb.append("</li>\n");
+			}
+			sb.append("</ul>\n");
+		}
+		sb.append("</ul></noinclude><includeonly>{{#switch:{{{1}}}");
+		for(var cat:categories) {
+			for(var e:cat.entries) {
+				sb
+					.append("|")
+					.append(e.key)
+					.append("=")
+					.append(Arrays.stream(e.bbox)
+						.map(BigDecimal::toPlainString)
+						.collect(Collectors.joining(","))
+					);
+			}
+		}
+		sb.append("|}}</includeonly>");
+		
+		
+		run.getWiki().editIfChange("Template:DisplayMap/Search", sb.toString(), "Automatic update");
+	}
+
 	private String key(Multiset<String> labelCounts, String label, String category) {
 		var key = label;
 		if(labelCounts.count(label)>1) {
@@ -114,6 +192,9 @@ public class MapSearchPage extends SimpleBot {
 		""";
 	}
 
+	record CategoryIn(String category, List<ResultIn> entries) {}
+	record ResultIn(String label, BigDecimal[] bbox, Double areaM2) {}
+	
 	record Category(String category, List<Result> entries) {}
-	record Result(String label, BigDecimal[] bbox) {}
+	record Result(String key, String label, int labelCount, BigDecimal[] bbox, Double areaM2) {}
 }
